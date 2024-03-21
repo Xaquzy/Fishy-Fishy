99b15c78b7ccb5310f40e2d7cad8110083e90e21
Xaquzy <143174417+Xaquzy@users.noreply.github.com>
2024-03-21 20:17:48 +0100

    Cutscene er lavet med placeholder

--- Assets/Scripts/CountDownTimer.cs
+++ Assets/Scripts/CountDownTimer.cs
@@ -9,8 +9,6 @@ using Cinemachine;
 public class CountDownTimer : MonoBehaviour
 {
     public float remainingTime = 5f;
-    public float messageWaitTime = 5f;
-    public float CutSceneWait = 5f;
     [SerializeField] TextMeshProUGUI countdownText;
     public GameObject ratingMessage;
     public GameObject Line; //for at få adgang til drawing script og dermed accuracy dist
@@ -19,6 +17,12 @@ public class CountDownTimer : MonoBehaviour
     public Transform Player;
     public Transform CutScenePos;
     public CinemachineFreeLook MainCam;
+    public GameObject CutSceneCam;
+    public GameObject CookCam;
+    public GameObject CutSceneTestText;
+    public float CutSceneTime = 5f;
+    public float RatingReadTime = 5f;
+    public GameObject knife; //Så den kan slukkes i cutscene
 
     //[SerializeField] private int NewSceneNumber = 1;
 
@@ -45,7 +49,9 @@ public class CountDownTimer : MonoBehaviour
         {
             remainingTime = 0; //Sætter tiden til 0 så timeren ikke kan blive negativ
             countdownText.color = Color.red; //Gør teksten rød
-            StartCoroutine(FinishCut()); //kalder på sceneskift funktionen
+            Debug.Log("Go to cutscene");
+            StartCoroutine(CutSceneInScene()); //kalder på sceneskift funktionen
+
 
         }
 
@@ -55,19 +61,7 @@ public class CountDownTimer : MonoBehaviour
 
     }
 
-    //Scenechange efter x sekunders cutscene
-    IEnumerator FinishCut()
-    {
-        Rating(); //kalder på rating funktionen når tiden er over, aka vurdering af ens cut
-        yield return new WaitForSeconds(messageWaitTime); //kode til lidt pause så man kan se beskeden
-
-        //RatingManager.DisplayRatings(); //Det er en test for at se om ratingen bliver gemt i listen som denne funktion printer
-        Debug.Log("Go to cutscene");
-        //To muligheder til cutscene
-        //Skift scene = SceneManager.LoadScene(NewSceneNumber);
-        //Skift kamera og afspil animation = at kalded på nedenstående funktion (funktionen er tom lige nu)
-        CutSceneInScene();
-    }
+ 
     void Rating()
     {
         Drawing drawing = Line.GetComponent<Drawing>(); //få adgang til drawing script
@@ -121,29 +115,56 @@ public class CountDownTimer : MonoBehaviour
 
         }
         rating.SetActive(true);
+
         RatingManager.AddRating(rating.name); //Konverter rating navn til string
 
     }
-    void CutSceneInScene()
+    IEnumerator CutSceneInScene()
     {
         //Slukker for movement script
         Movement movement = Player.GetComponent<Movement>(); 
         movement.enabled = false;
 
-        //Placer spilleren i det rigtige sted
+        //Slukker for Drawing script
+        Drawing drawing = Line.GetComponent<Drawing>();
+        drawing.enabled = false;
+
+        //Placer spilleren i det rigtige sted 
         Player.position = CutScenePos.position;
-        //CutSceneCam.enabled(true);
-        //MainCam.enabled(false);
+
+        //kniven skal slukkes
+        knife.SetActive(false);
+
+        //Camera ting
+        CookCam.SetActive(false);
+        CutSceneCam.SetActive(true);
+        MainCam.enabled = false;
+        CutSceneTestText.SetActive(true); //Det er bare en placeholder tester
+
+        //Den reelle cutscene
         //Afspil animationer
         //Afspil Lyd
-        //Vent = yield return new WaitForSeconds(CutSceneWait); //kode til lidt pause så man kan se beskeden
-        //MainCam.enabled(true);
-        //CutSceneCam.enabled(false);
+
+        yield return new WaitForSeconds(CutSceneTime); //kode til lidt pause så man kan se Cutscene før ratingen popper up
+        Rating();
+        yield return new WaitForSeconds(RatingReadTime); //Lidt tid til at læse sin rating
+
+        //Gør klar til fortsat spil
+        MainCam.enabled = true;
+        CutSceneCam.SetActive(false);
         //Skift fiske objektFunktion
         //Reset spillerens position i køkkenet?
 
-        //Tænder for movement script
-        //movement.enabled = true;
+        //Tænder for movement script og slukker for ratingen (og for Cutscenetext som er placeholder)
+        movement.enabled = true;
+        ratingMessage.SetActive(false);
+
+
+        //Sluk CountDownTimer script.. altså det script vi er i. Dette er så remaingTime kan genstartes til næste cut i fisken
+        enabled = false;
+
+        //FOR AT VISE ALLE RATINGS TIL SIDST
+        //RatingManager.DisplayRatings(); //Det er en test for at se om ratingen bliver gemt i listen som denne funktion printer
     }
 }
 
