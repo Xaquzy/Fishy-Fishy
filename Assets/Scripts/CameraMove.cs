2a68b3e566e71b867636701062736d4de5e1c43b
byggemandb <basmalayj@hotmail.com>
2024-04-18 01:08:39 +0200

    Læs desc - Tutorial camera gør sin ting
    Tutorial scene er i gang, audio og UI bliver lavet lidt på samme tid.
    Nest thing i shall do is time certain lines with certain position of the tutcam, which is easy enough.

--- Assets/Scripts/CameraMove.cs
+++ Assets/Scripts/CameraMove.cs
@@ -3,25 +3,18 @@ using UnityEngine;
 public class CameraMove : MonoBehaviour
 {
     public Transform[] waypoints; // Array to hold the waypoints
-    public float moveSpeed = 5f; // Speed at which the camera moves
+    public float moveSpeed = 5f; 
 
     private int currentWaypointIndex = 0;
 
     void Update()
     {
-        // Check if there are waypoints
-        if (waypoints.Length == 0)
-        {
-            Debug.LogWarning("No waypoints assigned to CameraPath script.");
-            return;
-        }
-
-        // Move towards the current waypoint
         Transform currentWaypoint = waypoints[currentWaypointIndex];
         transform.position = Vector3.MoveTowards(transform.position, currentWaypoint.position, moveSpeed * Time.deltaTime);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, currentWaypoint.rotation, moveSpeed * Time.deltaTime);
 
         // Check if the camera has reached the current waypoint
-        if (transform.position == currentWaypoint.position)
+        if (transform.position == currentWaypoint.position ||transform.rotation==currentWaypoint.rotation)
         {
             // Move to the next waypoint
             currentWaypointIndex = (currentWaypointIndex + 1) % waypoints.Length;
