425493678d68d504a4ab25179d6d279fd6ade52c
byggemandb <basmalayj@hotmail.com>
2024-04-18 17:43:09 +0200

    Tut

--- Assets/Scripts/CameraMove.cs
+++ Assets/Scripts/CameraMove.cs
@@ -1,9 +1,11 @@
+using Cinemachine;
+using System.Collections;
 using UnityEngine;
 
 public class CameraMove : MonoBehaviour
 {
     public Transform[] waypoints; // Array to hold the waypoints
-    public float moveSpeed = 5f; 
+    public float moveSpeed = 5f;
 
     private int currentWaypointIndex = -1;
     private float distance;
@@ -18,13 +20,19 @@ public class CameraMove : MonoBehaviour
     public GameObject Sub4;
     public GameObject Sub5;
     public GameObject Sub6;
+    public float LastPointTime = 2f;
+    public GameObject TutorialCam;
+    public CinemachineFreeLook MainCam;
+
+    private bool isMoving = true;
+
     private void Start()
     {
         NextPoint();
 
         subParent.SetActive(true);
 
-        
+
         for (int i = 1; i < subParent.transform.childCount; i++)
         {
             Transform sub = subParent.transform.GetChild(i);
@@ -34,6 +42,11 @@ public class CameraMove : MonoBehaviour
 
     void Update()
     {
+        if (!isMoving)
+        {
+            return;
+        }
+
         transform.position = Vector3.MoveTowards(transform.position, currentWaypoint.position, moveSpeed * Time.deltaTime);
         float distanceTravelled = (transform.position - position).magnitude;
         transform.rotation = Quaternion.Slerp(rotation, currentWaypoint.rotation, distanceTravelled / distance);
@@ -46,7 +59,7 @@ public class CameraMove : MonoBehaviour
         }
 
         Debug.Log("Current position: " + transform.position);
-        if (Vector3.Distance(transform.position, new Vector3(187.286362f, 204.089996f, 197.217484f)) < 0.03f)
+        if (Vector3.Distance(transform.position, new Vector3(185.860565f, 204.089996f, 195.037949f)) < 0.03f)
         {
             Sub1.SetActive(false);
             Sub2.SetActive(true);
@@ -56,7 +69,7 @@ public class CameraMove : MonoBehaviour
             Sub2.SetActive(false);
             Sub3.SetActive(true);
         }
-        if (Vector3.Distance(transform.position, new Vector3(215.976974f, 204.089996f, 213.968185f))<0.03f)
+        if (Vector3.Distance(transform.position, new Vector3(214.567444f, 204.089996f, 214.730057f)) < 0.03f)
         {
             Sub3.SetActive(false);
             Sub4.SetActive(true);
@@ -66,14 +79,22 @@ public class CameraMove : MonoBehaviour
             Sub4.SetActive(false);
             Sub5.SetActive(true);
         }
-        if (Vector3.Distance(transform.position, new Vector3(215.919952f, 204.089996f, 188.116257f)) < 0.03f)
+        if (Vector3.Distance(transform.position, new Vector3(215.903076f, 204.089996f, 191.234177f)) < 0.03f)
         {
             Sub5.SetActive(false);
             Sub6.SetActive(true);
         }
+         if (Vector3.Distance(transform.position, new Vector3(215.929993f, 204.089996f, 186.25f)) < 0.03f)
+        {
+            Debug.Log("JOJFNFE");
+            isMoving = false; // Stop the camera movement
+            StartCoroutine(SlukSidsteSub());
+
+        }
+
 
     }
-    
+
 
     void NextPoint()
     {
@@ -83,7 +104,12 @@ public class CameraMove : MonoBehaviour
         rotation = transform.rotation;
         position = transform.position;
     }
+
+    IEnumerator SlukSidsteSub()
+    {
+        yield return new WaitForSeconds(LastPointTime);
+        Sub6.SetActive(false);
+        yield return new WaitForSeconds(0.1f);
+        TutorialCam.SetActive(false);
+    }
 }
-    
-
-
---
+++ Assets/Scripts/TutCookZone.cs
@@ -0,0 +1,194 @@
+using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutCookZone : MonoBehaviour
+{
+    public GameObject CookCam;
+    public CinemachineFreeLook MainCam;
+    public Transform CookPos;
+    public GameObject toolTip;
+    public GameObject Line;
+    public GameObject countdownText;
+    public GameObject Player;
+    public GameObject ratingMessage;
+    private bool playerInTrigger = false;
+    private bool InCookMode = false;
+
+    //kalde på countdowntimer scriptet
+    public CountDownTimer CountDownTimer;
+
+
+    //kalde på alle egenskabsscripts og deres tilhørende objekter
+    public Drawing Drawingscript;
+    public GameObject knife;
+    public GameObject Modelkniv;
+
+    public RemoveOrgans RemoveOrgansscript;
+    public GameObject Hand;
+
+    public WaterHose WaterHosescript;
+    public GameObject hose;
+    public GameObject hoseModel;
+
+
+
+    private void Start()
+    {
+        //Sluk for musen
+        Cursor.visible = false;
+
+        // Sluk egenskabsscriptne
+        SlukEgenskaber();
+
+
+        toolTip.SetActive(false); //Sluk tooltip
+        CookCam.SetActive(false); //Sluk cookcam
+        ratingMessage.SetActive(false); //sluk ratings
+
+        //CountdownText objektet slukkes (derved slukkes countdown scriptet på det også)
+        countdownText.SetActive(false);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        // Check if the object that entered the trigger has a specific tag
+        if (other.CompareTag("Player"))
+        {
+            toolTip.SetActive(true);
+            playerInTrigger = true;
+        }
+    }
+
+    private void Update()
+    {
+        // Check if the player is in the trigger zone and pressed the "E" key
+        if (playerInTrigger && Input.GetKeyDown(KeyCode.Space))
+        {
+            InCookMode = true;
+            Cursor.visible = true;
+            toolTip.SetActive(false);
+            CookCam.SetActive(true);
+            MainCam.enabled = false;
+            CountDownTimer.enabled = true; //slå den til mens man er i gang med din handling
+            Debug.Log("the countdown timer script is now turned on");
+
+
+
+        }
+
+        if (InCookMode == true)
+        {
+            //Vælg egenskabsfunktion
+            ChooseEgenskab();
+        }
+        if (playerInTrigger == false)
+        {
+            toolTip.SetActive(false);
+            CookCam.SetActive(false);
+            MainCam.enabled = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        // Check if the object that entered the trigger has a specific tag
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Player left the trigger!");
+            playerInTrigger = false;
+            InCookMode = false;
+            //Drawing drawing = Line.GetComponent<Drawing>();
+            Drawingscript.enabled = false;
+
+            //Objekterne der er vigtige til egenskaberne skal 
+            knife.SetActive(false);
+            hose.SetActive(false);
+            Hand.SetActive(false);
+
+            //CountDownTimer countDown = Player.GetComponent<CountDownTimer>(); //hente countdowntimer scriptet fra spilleren
+            CountDownTimer.enabled = false; ; //stoppe scriptet når man forlader skære mode
+            Debug.Log("the countdown timer script is now turned off");
+        }
+    }
+
+    public void SlukEgenskaber()
+    {
+        if (Drawingscript != null)
+        {
+            //sluk tegne script
+            Drawingscript.enabled = false;
+            knife.SetActive(false);
+        }
+
+        if (RemoveOrgansscript != null)
+        {
+            //sluk GrabObject script
+            RemoveOrgansscript.enabled = false;
+            Hand.SetActive(false);
+        }
+
+        if (WaterHosescript != null)
+        {
+            //Sluk vand
+            WaterHosescript.enabled = false;
+            hose.SetActive(false);
+        }
+    }
+
+    void ChooseEgenskab()
+    {
+        //Lav et tool tip der viser hvilket tal der svarer til hvilken egenskab
+        if (Input.GetKeyDown(KeyCode.Alpha1)) //Hvis man trykker på tallet 1
+        {
+            if (RemoveOrgansscript != null)
+            {
+                SlukEgenskaber(); //slukker alle og tænder en så man ikke har flere på samme tid
+
+                //Tænd GrabObject script
+                RemoveOrgansscript.enabled = true;
+                Hand.SetActive(true);
+
+                //Hosemodel
+                hoseModel.SetActive(true);
+
+                //kniv model
+                Modelkniv.SetActive(true);
+
+
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2)) //Hvis man trykker på tallet 2
+        {
+            if (Drawingscript != null)
+            {
+                SlukEgenskaber(); //slukker alle og tænder en så man ikke har flere på samme tid
+
+                //Tænd tegne script
+                Drawingscript.enabled = true;
+                knife.SetActive(true);
+                Modelkniv.SetActive(false);
+
+                //Hosemodel
+                hoseModel.SetActive(true);
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha3)) //Hvis man trykker på tallet 3
+        {
+            if (WaterHosescript != null)
+            {
+                SlukEgenskaber(); //slukker alle og tænder en så man ikke har flere på samme tid
+
+                //Tænd tegne script
+                WaterHosescript.enabled = true;
+                hose.SetActive(true);
+                hoseModel.SetActive(false);
+
+                //Kniv model
+                Modelkniv.SetActive(true);
+            }
+        }
+    }
+}
