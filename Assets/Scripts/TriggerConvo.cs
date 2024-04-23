d7149e570572ca158641c57b377b5bb0cbfe045b
byggemandb <basmalayj@hotmail.com>
2024-04-23 02:09:20 +0200

    Update TriggerConvo.cs

--- Assets/Scripts/TriggerConvo.cs
+++ Assets/Scripts/TriggerConvo.cs
@@ -6,18 +6,6 @@ public class TriggerConvo : MonoBehaviour
 {
     public AudioSource dialog;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log("Trigger entered by: " + other.name);
